bbd087ae14b86368d5869cf95a8a19a606056568
syha <syha@ucar.edu>
2023-03-28 10:03:05 -0600

    commit myconfig4psfc

---
+++ git.cmd
@@ -0,0 +1,20 @@
+(npl) work/MPAS-Workflow> git checkout 0a006fc
+Note: switching to '0a006fc'.
+
+You are in 'detached HEAD' state. You can look around, make experimental
+changes and commit them, and you can discard any commits you make in this
+state without impacting any branches by switching back to a branch.
+
+If you want to create a new branch to retain commits you create, you may
+do so (now or later) by using -c with the switch command. Example:
+
+  git switch -c <new-branch-name>
+
+Or undo this operation with:
+
+  git switch -
+
+Turn off this advice by setting config variable advice.detachedHead to false
+
+HEAD is now at 0a006fc Release 25JAN2023 (#190)
+
